82b4a9f0c475f20c33d2c31b0b586a1af0e999ff
Jesal Patel <jesalpatel87@yahoo.com>
2022-07-30 12:12:52 +0530

    Update user_route.go

--- routes/user_route.go
+++ routes/user_route.go
@@ -9,7 +9,7 @@ func UserRoute(router *mux.Router) {
 	router.HandleFunc("/alerts/create/{alertPrice}", controllers.CreateAlert()).Methods("GET")
 	router.HandleFunc("/alerts/delete/{alertPrice}", controllers.DeleteAlert()).Methods("DELETE")
 	router.HandleFunc("/alerts", controllers.GetAllAlerts()).Methods("GET")
-	router.HandleFunc("/alerts/{state}", controllers.GetAlertsState()).Methods("GET")
+	router.HandleFunc("/alerts/show/{state}", controllers.GetAlertsState()).Methods("GET")
 	//router.HandleFunc("/user/{userId}", controllers.EditAUser()).Methods("PUT")
 
 }
